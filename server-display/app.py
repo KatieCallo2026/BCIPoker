5b37c1b3d83a1691456899df1c40d9a55b98ab5c
Abril Aguilar Lopez <abril124@ucla.edu>
2025-04-17 14:05:27 -0700

    add: better starting state logging

--- server-display/app.py
+++ server-display/app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-import threading
-import eventlet
-eventlet.monkey_patch()
-
 #################################################
 # app.py - flask server to display both streams
 ################################################
+
+import eventlet
+eventlet.monkey_patch() # must be the first import
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import threading
 
 from config import USE_MOCK_EEG, USE_MOCK_GSR  # import flags from config
 # can toggle both eeg and gsr separately 
@@ -21,6 +22,7 @@ if USE_MOCK_EEG:
 else:
     from eeg_client import stream_eeg
 
+print("🔧 Starting app.py...")
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
@@ -31,4 +33,5 @@ def index():
 if __name__ == "__main__":
     threading.Thread(target=stream_gsr, args=(socketio,), daemon=True).start()
     threading.Thread(target=stream_eeg, args=(socketio,), daemon=True).start()
+    print("🟢 Flask-SocketIO server is about to start")
     socketio.run(app, host="0.0.0.0", port=5000)
