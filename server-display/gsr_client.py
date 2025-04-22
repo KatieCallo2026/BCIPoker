0e274f843acb6c7066561cfae4dbc7a300eefcf8
abril <abril_al@yahoo.com>
2025-04-22 00:07:42 -0700

    bruh

--- server-display/gsr_client.py
+++ server-display/gsr_client.py
@@ -9,22 +9,24 @@ import serial
 def stream_gsr(socketio,useSocketio=True):
     '''Stream gsr data to serial port'''
     
-    # In Windows Powershell:
-    # usbipd list
-    # usbipd bind --busid N-N
-    # usbipd attach --wsl --busid N-N
-    # usbipd list <- verify its attached and shared
-    
-    # To unbind (for BT setup)
-    # usbipd unbind --busid N-N
+    '''In Windows Powershell:
+    usbipd list
+    usbipd bind --busid N-N
+    usbipd attach --wsl --busid N-N
+    usbipd list 
+            ^ verify its attached and shared
+
+    To unbind (for BT setup)
+    usbipd unbind --busid N-N
+    ''' 
 
     ser = serial.Serial(
         #port='/dev/cu.usbmodemDC5475E9EE282',
         port='/dev/ttyACM0', # wsl connect to win 
         baudrate=9600,
-        parity=serial.PARITY_ODD,
-        stopbits=serial.STOPBITS_TWO,
-        bytesize=serial.SEVENBITS
+        #parity=serial.PARITY_ODD,
+        #stopbits=serial.STOPBITS_TWO,
+        #bytesize=serial.SEVENBITS
     )
 
     while(True):
