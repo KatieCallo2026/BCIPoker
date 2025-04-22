f19069094ce741d8c2c955400d4d7f44cd3dec1a
abril <abril_al@yahoo.com>
2025-04-21 23:18:44 -0700

    fix: gsr works on windows wsl, add: ucla logo, save data from session

--- server-display/gsr_client.py
+++ server-display/gsr_client.py
@@ -1,12 +1,26 @@
-import pyfirmata
+#import pyfirmata
 import time
 from datetime import datetime
 from config import GSR_UPDATE_INTERVAL
 import serial
+# !pip install bleak
 
-def stream_gsr(socketio,useSocketio=True):
+
+def stream_gsr_usb(socketio,useSocketio=True):
+    '''Stream gsr data to serial port'''
+    
+    # In Windows Powershell:
+    # usbipd list
+    # usbipd bind --busid N-N
+    # usbipd attach --wsl --busid N-N
+    # usbipd list <- verify its attached and shared
+    
+    # To unbind (for BT setup)
+    # usbipd unbind --busid N-N
+
     ser = serial.Serial(
-        port='/dev/cu.usbmodemDC5475E9EE282',
+        #port='/dev/cu.usbmodemDC5475E9EE282',
+        port='/dev/ttyACM0', # wsl connect to win 
         baudrate=9600,
         parity=serial.PARITY_ODD,
         stopbits=serial.STOPBITS_TWO,
